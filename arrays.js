cb23a5713cb615e2c7e9aaa2cbed46f15d0ddcc8
Alice Ly Ms <alicehly@gmail.com>
2019-05-10 03:14:14 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,21 @@
+var chocolateBars = ['snickers', 'hundred grand', 'kit kat', 'skittles']
+
+function addElementToBeginningOfArray(array, element) {
+  return[element,...array];
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  array.unshift(element)
+  return array
+}
+
+function addElementToEndOfArray(array, element){
+  array.push(element)
+  return array
+}
+
+function destructivelyAddElementToEndOfArray(array, element){
+  array.push(element)
+  return array
+}
+
